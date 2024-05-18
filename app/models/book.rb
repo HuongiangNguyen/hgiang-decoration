6d20bdf9f0845ecff7e152df44283074cecdfcd8
HuongiangNguyen <huongiangngth@gmail.com>
2024-05-19 00:17:19 +0700

    Polishing plate boiler content

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,7 +1,3 @@
 class Book < ApplicationRecord
   belongs_to :subject
-  
-  validates_presence_of :title, :author, :publisher
-  validates_uniqueness_of :title
-  validates_numericality_of :year
 end
